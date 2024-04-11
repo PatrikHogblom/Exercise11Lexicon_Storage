62076339995f552c194944b03cbdb1d03295560f
PatrikHogblom <patrik.hogblom@live.se>
2024-04-11 14:18:57 +0200

    added data annotations, for wxample to restrict the users input

--- Storage/Models/Product.cs
+++ Storage/Models/Product.cs
@@ -1,13 +1,23 @@
-﻿namespace Storage.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
 {
     public class Product
     {
+        [Key]
         public int Id { get; set; }
+        [StringLength(20, ErrorMessage = "Product name can contain only 20 characters")]
         public string Name { get; set; } = string.Empty;
+
+        [Range(10,100000, ErrorMessage ="Value must be between {1} and {2}.")]
+        [DataType(DataType.Currency)]
         public int Price { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime OrderDate { get; set; }
-        public string Category { get; set; }
+        public string? Category { get; set; }
         public string Shelf { get; set; }
+
         public int Count { get; set; }
         public string Description { get; set; }
     }
